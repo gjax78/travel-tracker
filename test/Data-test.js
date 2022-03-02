6738ad95c7bf6f910f153be6562be1cc9e334c8c
Geena Jackson <gjacks303@gmail.com>
2022-03-02 12:13:55 -0700

    Refactor data set to see if quotes affected functionality

--- test/Data-test.js
+++ test/Data-test.js
@@ -82,12 +82,12 @@ const testData = {
 
   "destinations": [
     {
-      "id": 1,
-      "destination": "Lima, Peru",
-      "estimatedLodgingCostPerDay": 70,
-      "estimatedFlightCostPerPerson": 400,
-      "image": "https://images.unsplash.com/photo-1489171084589-9b5031ebcf9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80",
-      "alt": "overview of city buildings with a clear sky"
+      id: 1,
+      destination: "Lima, Peru",
+      estimatedLodgingCostPerDay: 70,
+      estimatedFlightCostPerPerson: 400,
+      image: "https://images.unsplash.com/photo-1489171084589-9b5031ebcf9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80",
+      alt: "overview of city buildings with a clear sky"
     },
     {
       "id": 2,
