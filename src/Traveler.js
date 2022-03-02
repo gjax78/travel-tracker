df1b5b56d941c51c5eaa79ce1a0c399e612c2247
Geena Jackson <gjacks303@gmail.com>
2022-03-02 10:05:36 -0700

    Create Traveler class

---
+++ src/Traveler.js
@@ -0,0 +1,11 @@
+class Traveler {
+  constructor(traveler) {
+    this.id = traveler.id
+    this.name = traveler.name
+    this.type = traveler.travelerType
+    this.trips = []
+  }
+}
+
+
+export default Traveler;
