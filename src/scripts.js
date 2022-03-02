7f422bba9d57573186a39781cf45671eafde2954
Geena Jackson <gjacks303@gmail.com>
2022-03-02 15:10:36 -0700

    Create basic promise all structure

--- src/scripts.js
+++ src/scripts.js
@@ -1,6 +1,25 @@
-
+import fetchData from './apiCalls'
 import './css/styles.css';
 import './images/turing-logo.png';
 import Traveler from './Traveler';
 import Destination from './Destination';
 import Trip from './Trip';
+
+
+
+//-----------------------functions ---------------//
+
+const renderPage = () => {
+  Promise.all([fetchData('travelers'), fetchData('trips'), fetchData('destinations')])
+  .then(values => {
+    //invoke a function that does the instantiations (initialized user data)
+  })
+}
+
+// do fetch
+// do promise.all
+//create domUpdates folder
+//display user's trips
+
+
+//separate function that calls travelers to do my fetch (based on user id)
