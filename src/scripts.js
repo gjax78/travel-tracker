a24d131da36756e250604cc7f44b890c31e39002
Geena Jackson <gjacks303@gmail.com>
2022-03-03 14:56:55 -0700

    Create trip

--- src/scripts.js
+++ src/scripts.js
@@ -5,19 +5,31 @@ import Traveler from './Traveler';
 import Destination from './Destination';
 import Trip from './Trip';
 
-
+//-----------------------global variables ---------------//
+let currentTraveler;
 
 //-----------------------functions ---------------//
 
+//generating the destinations by bringing in the traveler from the api
+//the travelerRawData is the entire API information
 const generateNewTraveler = (travelerRawData) => {
-  let currentTraveler = new Traveler(travelerRawData)
-  return currentTraveler
+  currentTraveler = new Traveler(travelerRawData)
+  // return currentTraveler
 }
 
-const findTravelerTrips = (tripRawData) => {
-  let newTrip = new Trip(tripRawData)
+//generating the destinations by bringing in the trips from the api
+//the tripRawData is the entire API information
+const generateTravelerTrips = (tripRawData) => {
+  // console.log(tripTrawData)
+  tripRawData.trips.forEach(trip => {
+    let newTrip = new Trip(trip)
+    currentTraveler.travelerAllTrips(newTrip)
+    console.log(newTrip)
+  })
 }
 
+//generating the destinations by bringing in the destinations from the api
+//the destinationRawData is the entire API information
 const generateTripDestinations = (destinationRawData) => {
   let newDestination = new Destination(destinationRawData)
 }
@@ -29,29 +41,33 @@ const renderPage = () => {
   Promise.all([travelerPromise, tripPromise, destinationPromise])
   .then(values => {
     generateNewTraveler(values[0])
-    findTravelerTrips(values[1])
+    generateTravelerTrips(values[1])
     generateTripDestinations(values[2])
     // console.log(values[0].travelers[0])
   })
 }
 
-renderPage()
+
+
+
+
+
+//----------------------------scripts -----------------
+window.onload = (event) => (event, renderPage());
+
+
+
+
+
+
+
+
+
+
+
+//-----------------------------notes----------------------
 // do fetch
 // do promise.all
 //create domUpdates folder
 //display user's trips
 //separate function that calls travelers to do my fetch (based on user id)
-
-//
-// const generateNewTraveler = (travelerRawData) => {
-//   let currentTraveler = new Traveler(travelerRawData)
-//   console.log(travelerRawData)
-// }
-//
-// const findTravelerTrips = (tripRawData) => {
-//   let newTrip = new Trip(tripRawData)
-// }
-//
-// const generateTripDestinations = (destinationRawData) => {
-//   let newDestination = new Destination(destinationRawData)
-// }
