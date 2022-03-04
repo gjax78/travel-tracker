bf5002003410afa7094da6b165beeb598f11df6f
Geena Jackson <gjacks303@gmail.com>
2022-03-03 19:58:01 -0700

    Update traveler method to find the trip

--- src/Traveler.js
+++ src/Traveler.js
@@ -1,3 +1,6 @@
+import Trip from './Trip'; //bringing this in instead of line 36 in scripts
+
+
 class Traveler {
   constructor(travelerRawData) {
     this.id = travelerRawData.id
@@ -6,11 +9,28 @@ class Traveler {
     this.trips = []
   }
 
-  travelerAllTrips(newTrip) {
-    if (newTrip.userId === this.id) {
-      this.trips.push(newTrip)
-    }
-  }
+  travelerAllTrips(trip) {
+    // let newTrip = new Trip(trip)
+    if (trip.userID === this.id) {
+         this.trips.push(new Trip(trip))
+       }
+       // console.log(this.trips)
+     }
+    // let tripLog = newTrip.forEach(trip => {
+    //   if (trip.userId === this.id) {
+    //     this.trips.push(newTrip)
+    //   }
+    // })
+    // console.log(this.trips)
+    // if (newTrip.userId === this.id) {
+    //   this.trips.push(newTrip)
+    //   console.log(this.trips)
+    // }
+  // }
+
+  // getDestinations() {
+  //   if (trip.destinationId === this.id)
+  // }
 }
 
 export default Traveler;
