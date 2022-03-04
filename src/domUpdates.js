899bb4048a9b32ff3b0c534a30d13447a4c9d685
Geena Jackson <gjacks303@gmail.com>
2022-03-04 12:11:15 -0700

    Move method to pull trip data

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,5 +1,8 @@
 const welcomeMessage = document.querySelector('.welcome-text')
 const displayTrips = document.querySelector('.trips')
+const cardImage = document.querySelector('.card-image')
+const cardSection = document.querySelector('.card-grid')
+
 
 
 let domUpdates = {
@@ -8,8 +11,28 @@ let domUpdates = {
   },
 
   updateTrips(trips) {
-    console.log(trips)
-    displayTrips.innerText = `${trips[0].date}`
+    // console.log(trips)
+    //empty out the all-cards div (assign innerHTML to ' ')
+    cardSection.innerHTML = ' '
+    trips.forEach(trip => {
+      cardSection.innerHTML +=
+      `
+      <article class="card">
+        <h3 class="card-destination">${trip.destination.name}</h3>
+        <img class="card-image" src="${trip.destination.image}" alt="alt-text">
+        <p class="card-travelers"></p>
+        <p class="card-date"></p>
+        <p class="card-duration"></p>
+        <p class="card-lodging"></p>
+        <p class="card-flights"></p>
+        <p class="card-status"></p>
+      </article>
+      `
+      //you're going to have a different number of trips each time
+      // displayTrips.innerText = `${trip.destination.name}`
+      // displayTripDate.innerText = `${trip.date}`
+      // cardImage.src = `${trip.destination.image}`
+    })
   }
 
 
