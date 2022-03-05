5294e4b329afebbe7ba8f0118df0940820c5d495
Geena Jackson <gjacks303@gmail.com>
2022-03-04 17:53:59 -0700

    Wrap api calls in a function

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,13 +1,52 @@
-const fetchData = (extension) => {
-  return fetch(`http://localhost:3001/api/v1/${extension}`)
-    .then(response => response.json())
-    .catch(err => console.log(err))
-};
+const fetchAPI = {
+  fetchData(extension) {
+    return fetch(`http://localhost:3001/api/v1/${extension}`)
+      .then(response => response.json())
+      .catch(err => console.log(err))
+  },
+
+  postData(extension, tripRequest) {
+    return fetch(`http://localhost:3001/api/v1/${extension}`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(tripRequest)}
+    })
+    .then(response => {
+      if(!response.ok) {
+        throw new Error('Please fill out all input fields!')
+      } else {
+        alert("Your trip has been submitted. Please await the agent's approval or denial.")
+        return response.json()
+      }
+    })
+  }
+}
 
 
-// const postData = () => {
-//   return fetch(``)
+
+
+//
+// const fetchData = (extension) => {
+//   return fetch(`http://localhost:3001/api/v1/${extension}`)
+//     .then(response => response.json())
+//     .catch(err => console.log(err))
+// };
+//
+//
+// const postData = (extension) => {
+//   return fetch(`http://localhost:3001/api/v1/${extension}`, {
+//     method: 'POST',
+//     headers: { 'Content-Type': 'application/json' },
+//     body: JSON.stringify(newHydro)}
+//   })
+//   .then(response => {
+//     if(!response.ok) {
+//       throw new Error('Please fill out all input fields!')
+//     } else {
+//       alert("Your trip has been submitted. Please await the agent's approval or denial.")
+//       return response.json()
+//     }
+//   })
 // }
 
-
-export default fetchData;
+export default fetchAPI;
