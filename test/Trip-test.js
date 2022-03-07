a3d4da2c0288e732c70435724db5dd9311ba3fe3
Geena Jackson <gjacks303@gmail.com>
2022-03-07 14:55:49 -0700

    Make trip test more robust

--- test/Trip-test.js
+++ test/Trip-test.js
@@ -30,26 +30,43 @@ describe('Trip', () => {
 
   it('should have an id', () => {
     expect(trip3.id).to.equal(83);
+    expect(trip1.id).to.equal(1);
+    expect(trip4.id).to.equal(4);
+    expect(trip2.id).to.equal(2);
   })
 
   it('should have a user that booked the trip', () => {
+    expect(trip4.userId).to.equal(43);
     expect(trip3.userId).to.equal(1);
+    expect(trip2.userId).to.equal(1);
   })
 
   it('should have a destination id', () => {
     expect(trip1.destinationId).to.equal(49);
+    expect(trip4.destinationId).to.equal(14);
+    expect(trip2.destinationId).to.equal(25);
+    expect(trip5.destinationId).to.equal(29);
   })
 
   it('should state how many travelers there are', () => {
     expect(trip4.travelers).to.equal(2);
+    expect(trip2.travelers).to.equal(5);
+    expect(trip1.travelers).to.equal(1);
   })
 
   it('should have a starting date', () => {
     expect(trip4.date).to.equal("2022/02/25");
+    expect(trip5.date).to.equal("2022/04/30");
+    expect(trip2.date).to.equal("2022/10/04");
+    expect(trip1.date).to.equal("2022/09/16");
   })
 
   it('should have a duration', () => {
+    expect(trip1.duration).to.equal(8);
+    expect(trip2.duration).to.equal(18);
     expect(trip3.duration).to.equal(11);
+    expect(trip4.duration).to.equal(10);
+    expect(trip5.duration).to.equal(18);
   })
 
   it('should have a status', () => {
@@ -59,6 +76,7 @@ describe('Trip', () => {
 
   it('should have a list of suggested activities', () => {
     expect(trip3.suggestedActivities).to.eql([]);
+    expect(trip4.suggestedActivities).to.eql([]);
+    expect(trip5.suggestedActivities).to.eql([]);
   })
-
 });
